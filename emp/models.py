cc619e0f2f2ce962169f799a168616bc7590ce3f
smrdc <lishaojun@supermap.com>
2017-11-25 11:37:21 +0800

    lesson 05.2

--- emp/models.py
+++ emp/models.py
@@ -9,10 +9,21 @@ class Dept(models.Model):
     dept_no = models.IntegerField(verbose_name='部门编号')
     name = models.CharField(max_length=50, verbose_name='部门名称')
 
+    def __str__(self):
+        return self.name
+
+
 
 class Emp(models.Model):
     emp_no = models.IntegerField(verbose_name='工号')
     name = models.CharField(max_length=10, verbose_name='姓名')
     gender = models.CharField(max_length=2, verbose_name='性别')
     dept = models.ForeignKey(Dept,verbose_name='部门', on_delete=models.CASCADE)
+    email = models.CharField(max_length=25, blank=True, null=True, verbose_name='邮箱')
+
+    def __str__(self):
+        return self.name
+
+
+
 
